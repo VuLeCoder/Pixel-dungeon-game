97897241c265e4eb81d4f5a6fc662314a65bd588
VuzLe <tle63917@gmail.com>
2026-03-11 23:49:09 +0700

    refactor(scene): update asset management for hero scene

--- src/engine/asset_manager.cpp
+++ src/engine/asset_manager.cpp
@@ -4,6 +4,8 @@ std::unordered_map<std::string, Texture2D> AssetManager::textures;
 
 void AssetManager::Load() {
     textures["start_scene"] = LoadTexture("assets/start_scene.png");
+    textures["hero_select_scene"] = LoadTexture("assets/hero_select_scene.png");
+    textures["start_button"] = LoadTexture("assets/start_button.png");
 }
 
 Texture2D AssetManager::GetTexture(std::string name) {
