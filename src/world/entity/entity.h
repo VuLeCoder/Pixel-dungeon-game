209d43a4152134f71fd32719075b8398a7e9004a
VuzLe <tle63917@gmail.com>
2026-03-30 18:12:54 +0700

    feat(entity): implement base class entity

--- src/world/entity/entity.h
+++ src/world/entity/entity.h
@@ -1,3 +1,30 @@
 #pragma once
+#include "raylib.h"
 
-class Entity {};
+class World;
+
+class Entity {
+protected:
+    World* world;
+    Vector2 pos;
+    bool alive = true;
+
+public:
+    Entity(float x, float y, World* world) : pos({x, y}), world(world) {}
+    virtual ~Entity() {}
+
+    bool isAlive() const { return alive; }
+    void destroy() { alive = false; }
+
+    Vector2 getPosition() { return pos; }
+    void setPos(float nx, float ny) { pos = {nx, ny}; }
+    World* getWorld() { return world; }
+
+    virtual bool isBlocking() const = 0;
+    virtual bool isPlayer() const = 0;
+    
+    virtual void takeDamage(int damage) = 0;
+    virtual void fall() = 0;
+    virtual void update(float dt) {}
+    virtual void render() = 0;
+};
