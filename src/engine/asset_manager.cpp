91229562ef484e5a8e7955f55effdef629d8aab0
VuzLe <tle63917@gmail.com>
2026-04-02 12:32:07 +0700

    feat(assets): add method for load animation refactor: clean code

--- src/engine/asset_manager.cpp
+++ src/engine/asset_manager.cpp
@@ -1,10 +1,35 @@
 #include "asset_manager.h"
-#include <sstream>
 
+#include <sstream>
+#include <fstream>
+#include <limits>
+
+// private:
 std::unordered_map<std::string, Texture2D> AssetManager::textures;
 std::unordered_map<std::string, std::vector<std::vector<int>>> AssetManager::dataMap;
+std::unordered_map<std::string, Animation> AssetManager::dataAnim;
 
-static std::vector<std::vector<int>> LoadCSV(const char* fileName) {
+
+void AssetManager::LoadAllTexture() {
+    struct TextureEntry {
+        std::string_view key;
+        std::string_view path;
+    };
+
+    TextureEntry entries[] = {
+        {START_SCENE, "assets/start_scene.png"},
+        {HERO_SCENE, "assets/hero_select_scene.png"},
+        {START_BUTTON, "assets/start_button.png"},
+        {TILE, "assets/tiles.png"}
+    };
+
+    for (auto& e : entries) {
+        textures[std::string(e.key)] = LoadTexture(e.path.data());
+    }
+}
+
+
+std::vector<std::vector<int>> AssetManager::LoadCSV_Map(const char* fileName) {
     std::vector<std::vector<int>> result;
 
     char* text = LoadFileText(fileName);
@@ -27,25 +52,94 @@ static std::vector<std::vector<int>> LoadCSV(const char* fileName) {
     return result;
 }
 
+void AssetManager::LoadAllLevelMap() {
+    struct MapEntry {
+        std::string_view key;
+        std::string_view path;
+    };
 
+    MapEntry entries[] = {
+        {"level1_Terrain", "assets/level/level1_Terrain.csv"},
+        {"level1_Door",    "assets/level/level1_Door.csv"},
+        {"level1_Trap",    "assets/level/level1_Trap.csv"}
+    };
+
+    for (auto& e : entries) {
+        dataMap[std::string(e.key)] = LoadCSV_Map(e.path.data());
+    }
+}
+
+Animation AssetManager::LoadAnimation(const std::string& fileName) {
+    Animation anim;
+    std::ifstream file(fileName);
+
+    int count; file >> count;
+    file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::string texturePath;
+    std::getline(file, texturePath);
+
+    textures[texturePath] = LoadTexture(texturePath.c_str());
+    Texture2D tex = textures[texturePath];
+
+    std::string line;
+    float x, y, w, h, dt;
+    for(int i=0; i<count; ++i) {
+        line = "";
+        while(line.empty() && std::getline(file, line));
+
+        file >> x >> y >> w >> h;
+        FrameImage f(tex, {x, y, w, h});
+        file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        file >> dt;
+        file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        anim.add(f, dt);
+    }
+
+    return anim;
+}
+
+void AssetManager::LoadAllAnimation() {
+     struct AnimEntry {
+        std::string_view entity;
+        std::string_view state;
+        std::string_view file;
+    };
+
+    AnimEntry entries[] = {
+        {WARRIOR, IDLE,         "assets/entity/creature/hero/warrior/idle_animation.txt"},
+        {WARRIOR, WALK,         "assets/entity/creature/hero/warrior/walk_animation.txt"},
+        {WARRIOR, ATK,          "assets/entity/creature/hero/warrior/attack_animation.txt"},
+        {WARRIOR, USE,          "assets/entity/creature/hero/warrior/use_animation.txt"},
+        {WARRIOR, USE_SCROLL,   "assets/entity/creature/hero/warrior/use_scroll_animation.txt"},
+        {WARRIOR, DEATH,        "assets/entity/creature/hero/death_animation.txt"}
+    };
+
+    for (auto& e : entries) {
+        std::string key = std::string(e.entity) + '_' + std::string(e.state);
+        dataAnim[key] = LoadAnimation(e.file.data());
+    }
+}
+
+//public:
 void AssetManager::Load() {
-    textures["start_scene"] = LoadTexture("assets/start_scene.png");
-    textures["hero_select_scene"] = LoadTexture("assets/hero_select_scene.png");
-    textures["start_button"] = LoadTexture("assets/start_button.png");
-
-    textures["tiles"] = LoadTexture("assets/tiles.png");
-
-    dataMap["level1_Terrain"] = LoadCSV("assets/level/level1_Terrain.csv");
-    dataMap["level1_Door"] = LoadCSV("assets/level/level1_Door.csv");
-    dataMap["level1_Trap"] = LoadCSV("assets/level/level1_Trap.csv");
+    LoadAllTexture();
+    LoadAllLevelMap();
+    LoadAllAnimation();
 }
 
-Texture2D AssetManager::GetTexture(std::string name) {
-    return textures[name];
+Texture2D& AssetManager::GetTexture(std::string_view name) {
+    return textures[std::string(name)];
 }
 
-std::vector<std::vector<int>>& AssetManager::GetMap(std::string name) {
-    return dataMap[name];
+std::vector<std::vector<int>>& AssetManager::GetMap(std::string_view name) {
+    return dataMap[std::string(name)];
+}
+
+Animation& AssetManager::GetAnimation(std::string_view name) {
+    return dataAnim[std::string(name)];
 }
 
 void AssetManager::Unload() {
--- src/engine/asset_manager.h
+++ src/engine/asset_manager.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "raylib.h"
+#include "./effect/animation.h"
+
 #include <unordered_map>
 #include <vector>
 #include <string>
@@ -8,11 +10,20 @@ class AssetManager {
 private:
     static std::unordered_map<std::string, Texture2D> textures;
     static std::unordered_map<std::string, std::vector<std::vector<int>>> dataMap;
+    static std::unordered_map<std::string, Animation> dataAnim;
+
+    static std::vector<std::vector<int>> LoadCSV_Map(const char* fileName);
+    static Animation LoadAnimation(const std::string& fileName);
+
+    static void LoadAllTexture();
+    static void LoadAllLevelMap();
+    static void LoadAllAnimation();
 
 public:
     static void Load();
     static void Unload();
 
-    static Texture2D GetTexture(std::string name);
-    static std::vector<std::vector<int>>& GetMap(std::string name);
+    static Texture2D& GetTexture(std::string_view name);
+    static std::vector<std::vector<int>>& GetMap(std::string_view name);
+    static Animation& GetAnimation(std::string_view name);
 };
