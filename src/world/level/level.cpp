c591f9ffdabad447476011388550436551bc09b4
VuzLe <tle63917@gmail.com>
2026-04-19 13:32:45 +0700

    refactor(level): break down the function into smaller parts for greater clarity.

--- src/world/level/level.cpp
+++ src/world/level/level.cpp
@@ -1,42 +1,58 @@
 #include "level.h"
 #include "tile_rule.h"
+#include "../../engine/asset_manager.h"
 
 #include <string>
 #include <iostream>
 
-std::string generateFileName(int depth) {
-    return "level" + std::to_string(depth);
+//private:
+MapData Level::loadMapData(int depth) {
+    std::string base = "level" + std::to_string(depth);
+
+    return {
+        AssetManager::GetMap(base + '_' + std::string(AssetManager::TERRAIN)),
+        AssetManager::GetMap(base + '_' + std::string(AssetManager::DOOR)),
+        AssetManager::GetMap(base + '_' + std::string(AssetManager::TRAP))
+    };
 }
 
-void Level::generateMap(int depth) {
-    std::string fileName = generateFileName(depth);
-    std::string terrainFile = fileName + "_Terrain";
-    std::string doorFile = fileName + "_Door";
-    std::string trapFile = fileName + "_Trap";
-   
-    std::vector<std::vector<int>> tileMatrix = AssetManager::GetMap(terrainFile);
-    std::vector<std::vector<int>> doorMatrix = AssetManager::GetMap(doorFile);
-    std::vector<std::vector<int>> trapMatrix = AssetManager::GetMap(trapFile);
+Tile Level::buildTile(int x, int y, const MapData& data) {
+    Tile tile;
+    int terrain = data.terrain[x][y];
 
-   
-    tiles.resize(MAP_SIZE, std::vector<Tile>(MAP_SIZE));
+    int trap = data.trap[x][y];
+    if(trap != NONE) {
+        tile.setTrapTile(terrain, trap == TRAP_HIDDEN);
+        return tile;
+    }
+
+    int door = data.door[x][y];
+    if(door != NONE) {
+        tile.setDoorTile(
+            door == DOOR_LOCKED,
+            door == DOOR_HIDDEN
+        );
+        return tile;
+    }
+
+    tile.setTerrainType(terrain);
+    return tile;
+}
+
+//public:
+Level::Level(World* world) : world(world) {}
+
+World* Level::getWorld() { return world; }
+
+
+void Level::generateMap(int depth) {
+    MapData data = loadMapData(depth);
+
+    tiles = std::vector<std::vector<Tile>>(MAP_SIZE, std::vector<Tile>(MAP_SIZE));
+
     for(int x=0; x<MAP_SIZE; ++x) {
         for(int y=0; y<MAP_SIZE; ++y) {
-
-            if(trapMatrix[x][y] != NONE) {
-                bool isHidden = (trapMatrix[x][y] == TRAP_HIDDEN);
-                tiles[x][y].setTrapTile(tileMatrix[x][y], isHidden);
-                continue;
-            }
-
-            if(doorMatrix[x][y] != NONE) {
-                bool isHidden = (doorMatrix[x][y] == DOOR_HIDDEN);
-                bool isLocked = (doorMatrix[x][y] == DOOR_LOCKED);
-                tiles[x][y].setDoorTile(isLocked, isHidden);
-                continue;
-            }
-
-            tiles[x][y].setTerrainType(tileMatrix[x][y]);
+            tiles[x][y] = buildTile(x, y, data);            
         }
     }
 }
@@ -55,4 +71,33 @@ void Level::render() {
             tiles[x][y].render(y, x);
         }
     }
+}
+
+
+bool Level::isBlockVision(int x, int y) const {
+    return tiles[x][y].isBlockVision();
+}
+#include <iostream>
+bool Level::isPassable(int x, int y) const {
+    if(x == 14 && y == 15) {
+        std::cout << tiles[x][y].isPassable() <<std::endl;
+    }
+    return tiles[x][y].isPassable();
+}
+
+bool Level::isDanger(int x, int y) const {
+    return tiles[x][y].isDanger();
+}
+
+
+void Level::onStep(Entity* e, int x, int y) {
+    tiles[x][y].onEnter(e, getWorld());
+}
+
+void Level::onLeft(int x, int y) {
+    tiles[x][y].onLeft();
+}
+
+Vector2 Level::getRandomFreeTile() {
+    
 }
--- src/world/level/level.h
+++ src/world/level/level.h
@@ -2,22 +2,36 @@
 
 #include <vector>
 #include <unordered_map>
-#include "../../utils/position.h"
 
 #include "../tile/tile.h"
 #include "../entity/creature/monster.h"
 #include "../entity/item/item.h"
 
+class World;
+
+struct MapData {
+    std::vector<std::vector<int>> terrain;
+    std::vector<std::vector<int>> door;
+    std::vector<std::vector<int>> trap;
+};
+
 class Level {
 private:
-    static const int MAP_SIZE = 33;
+    static constexpr int MAP_SIZE = 33;
+    World* world;
 
     std::vector<std::vector<Tile>> tiles;
-
     std::vector<Monster*> enemies;
     std::vector<Item*> items;
 
+    MapData loadMapData(int depth);
+    Tile buildTile(int x, int y, const MapData& data);
+
 public:
+    Level(World* world);
+
+    World* getWorld();
+
     void generateMap(int depth);
     void update();
     void render();
@@ -28,14 +42,14 @@ public:
     bool isPassable(int x, int y) const;
     bool isDanger(int x, int y) const;
 
-    void addEntity(Entity* e);
-    void removeEntity(Entity* e);
-    Entity* getEntityAt(Position pos);
+    void onStep(Entity* e, int x, int y);
+    void onLeft(int x, int y);
 
-    void wakeUpAllMonsters();
-    void spawnMonsterNear(Position pos);
+    // void addEntity(Entity* e);
+    // void removeEntity(Entity* e);
+    // Entity* getEntityAt(Vector2 pos);
 
-    Position getRandomFreeTile();
-
-    void onStep(Entity* e, Position pos);
+    // void wakeUpAllMonsters();
+    // void spawnMonsterNear(Vector2 pos);
+    Vector2 getRandomFreeTile();
 };
