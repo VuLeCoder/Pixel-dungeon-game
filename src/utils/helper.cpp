5b52ae32af74ff8e0c48b00effba1366197ba410
VuzLe <tle63917@gmail.com>
2026-04-21 19:42:42 +0700

    update(utils): safe distance function

--- src/utils/helper.cpp
+++ src/utils/helper.cpp
@@ -3,7 +3,7 @@
 const int TILE_SIZE = 16;
 
 int distance(const Vector2& pos1, const Vector2& pos2) {
-    float dx = (pos1.x - pos2.x) / TILE_SIZE;
-    float dy = (pos1.y - pos2.y) / TILE_SIZE;
-    return static_cast<int>(std::sqrt(dx * dx + dy * dy));
+    int dx = std::abs((int)(pos1.x / TILE_SIZE) - (int)(pos2.x / TILE_SIZE));
+    int dy = std::abs((int)(pos1.y / TILE_SIZE) - (int)(pos2.y / TILE_SIZE));
+    return std::max(dx, dy);
 }
